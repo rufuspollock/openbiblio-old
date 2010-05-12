735b7bf70b6ca838595ec18e9e47ea4ac6bac781
rgrp <http://rufuspollock.org@localhost>
2010-05-12 01:35:06 +0100

    [bugfix][xs]: use request.GET not request.params in lib/base.py __before__ as calling request.params appears to urlencode request.body causing unexpected behaviour (and problems) elsewhere.

--- openbiblio/lib/base.py
+++ openbiblio/lib/base.py
@@ -21,5 +21,7 @@ class BaseController(WSGIController):
     def __before__(self, action, **params):
         c.__version__ = openbiblio.__version__
         c.site_title = config.get('site_title', 'Bibliographica')
-        c.items_per_page = int(request.params.get('items_per_page', 20))
+        # WARNING: you must use request.GET as request.params appears to alter
+        # request.body (it gets url-encoded) upon call to request.params
+        c.items_per_page = int(request.GET.get('items_per_page', 20))
 
