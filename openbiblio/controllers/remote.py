fb572adfe44acc52520d6aa20c3ee1a8fdfc299e
William Waites <ww@ngwenya.styx.org>
2010-05-19 20:11:29 +0100

    fix import controller

--- openbiblio/controllers/remote.py
+++ openbiblio/controllers/remote.py
@@ -6,11 +6,21 @@ from ordf.pylons.graph import GraphControllerFactory
 from openbiblio.lib import base
 from openbiblio.lib.base import render, request, c, response
 from openbiblio import handler
+from logging import getLogger
+log = getLogger(__name__)
 
 # TODO: change this to be more elegant ...
 _RemoteController = GraphControllerFactory(base, handler)
 
 class RemoteController(_RemoteController):
+    def index(self):
+        try:
+            return super(RemoteController, self).index()
+        except:
+            from traceback import format_exc
+            log.info(format_exc())
+            return self._render()
+
     def _render(self):
         # stuff other things in c
         return render('graph_fetch.html')
