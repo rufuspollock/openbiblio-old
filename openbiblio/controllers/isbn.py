1ff1625fa25dac9b97987f3521e32e505a061f51
Graham Higgins <gjh@bel-epa.com>
2010-07-09 15:46:30 +0100

    Edit canned SPARQL query in ISBN search controller to work with both 4store and rdflib.

--- openbiblio/controllers/isbn.py
+++ openbiblio/controllers/isbn.py
@@ -10,14 +10,14 @@ PREFIX dc: <http://purl.org/dc/terms/>
 PREFIX obp: <http://purl.org/NET/obp/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-SELECT DISTINCT ?book, ?title, ?authorname, ?pubname, ?date%s
+SELECT DISTINCT ?book ?title ?authorname ?pubname ?date%s
 WHERE {
     ?book obp:isbn %s .
     ?book obp:work ?work .
     ?work dc:title ?title .
     OPTIONAL {
         ?book dc:date ?date
-    }
+    } .
     OPTIONAL {
         ?book dc:publisher ?publisher .
         ?publisher foaf:name ?pubname
@@ -32,7 +32,7 @@ WHERE {
 class IsbnController(SparqlController):
     def index(self, isbn=None):
         if isbn is None:
-            q = isbn_query % (", ?isbn", '?isbn') + "LIMIT 10"
+            q = isbn_query % (" ?isbn", '?isbn') + "LIMIT 10"
         else:
 	    if isbn.endswith(".json"):
                 request.GET["format"] = "application/sparql-results+json"
