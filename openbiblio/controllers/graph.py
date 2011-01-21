0ff1f3d5493aba6f1b2a90709d05cdd822f45f72
Ben O'Steen <bosteen@gmail.com>
2011-01-21 18:12:08 +0000

    Altered the graph controller to allow for '.bibtex' endings to get bibtex responses.

--- openbiblio/controllers/graph.py
+++ openbiblio/controllers/graph.py
@@ -5,9 +5,11 @@ RDF Graph Controller - see L{ordf.pylons.graph}.
 from pylons.controllers.util import abort
 from openbiblio.lib import base
 from openbiblio.lib.base import render, request, c, response
+from openbiblio.lib.bibtex import Bibtex
 from openbiblio import handler
 from ordf.onto.controllers.graph import GraphController as _GraphController
 from ordf.graph import Graph
+from ordf.term import URIRef
 
 construct_graph = """\
 DEFINE input:same-as "yes"
@@ -32,6 +34,11 @@ class GraphController(base.BaseController, _GraphController):
     def _get_graph(self):
         uri = self._uri()
         content_type, format = self._accept(uri)
+        if uri.endswith("bibtex"):
+            content_type = "text/x-bibtex"
+            format = "bibtex"
+            uri_str, _ = uri.rsplit(".", 1)
+            uri = URIRef(uri_str)
         graph = handler.get(uri)
         if len(graph) == 0:
             graph.rollback()
@@ -46,6 +53,13 @@ class GraphController(base.BaseController, _GraphController):
         if format == "html":
             c.graph = graph
             data = self._render_graph()
+        elif format == "bibtex":
+            b = Bibtex()
+            b.load_from_graph(graph)
+            data = b.to_bibtex()
+            response.content_type = str(content_type)
+            response.headers['Content-Location'] = "%s.bibtex" % b.uniquekey
+            response.headers['Location'] = "%s.bibtex" % b.uniquekey
         else:
             data = graph.serialize(format=format)
             response.content_type = str(content_type)
